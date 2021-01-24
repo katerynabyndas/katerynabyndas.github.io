c4085b0bcfc85d56ac75804ced56a35676e6704f
katerynabyndas <77918962+katerynabyndas@users.noreply.github.com>
2021-01-24 13:00:23 +0200

    vh

--- ecohome/js/main.js
+++ ecohome/js/main.js
@@ -1,4 +1,15 @@
-﻿//autoscroll
+﻿//resize func
+$(window).on('resize', function () {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+//autoscroll
 (function () {
     link = $('.js-scroll-link');
     link.on('click', function () {
